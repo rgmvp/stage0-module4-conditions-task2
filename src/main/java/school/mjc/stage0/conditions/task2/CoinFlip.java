ce807f16aceb404af24552ca293787feb937bc54
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-01-26 00:24:13 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        if (from1UpTo1000 < 500 && from1UpTo1000 >= 1){
+            System.out.println("Eagle");
+        }
+        else if (from1UpTo1000 >= 500 && from1UpTo1000 <= 1000){
+            System.out.println("Tail");
+        }
 
     }
 }
